166658148e14d8829f9bd67bc255b61064b219dd
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2021-04-22 21:38:51 +0900

    [인프런] 게임서버 초기 세팅

---
+++ DummyClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DummyClient
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Dummy Client");
+        }
+    }
+}
---
+++ ServerCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+    }
+}
