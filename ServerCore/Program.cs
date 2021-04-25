bd4f107b0744eb29659bd312901045beda22b489
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2021-04-25 20:54:24 +0900

    lock 잠그는 순서에 따른 Deadlock 발생

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,30 +4,66 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+    }
+
     internal class Program
     {
         static int number = 0;
         static object _obj = new object();
-        
+        static object _obj2 = new object();
+
         static void Thread_1()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                lock(_obj)
-                {
-                    number++;
-                }
+                SessionManager.Test();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                lock(_obj)
-                {
-                    number--;
-                }
+                UserManager.Test();
             }
         }
 
