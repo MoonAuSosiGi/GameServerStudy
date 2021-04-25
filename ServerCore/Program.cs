e46c75ec98995da0702bbed3c678be154ae6e325
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2021-04-25 12:20:00 +0900

    하드웨어 최적화에 의해 순서가 뒤바뀌는 현상

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,27 +6,42 @@ namespace ServerCore
 {
     internal class Program
     {
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        static void Thread_1()
+        {
+            y = 1; // Store y
+            r1 = x; // Load x
+        }
+
+        static void Thread_2()
+        {
+            x = 1; // Store x
+            r2 = y; // Load y
+        }
+
         public static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
-
+            int count = 0;
+            while (true)
             {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                    for (int x = 0; x < 10000; x++)
-                        arr[y, x] = 1;
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"[y,x] 걸린 시간 {end - now}");
-            }
+                count++;
+                x = y = r1 = r2 = 0;
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                    for (int x = 0; x < 10000; x++)
-                        arr[x, y] = 1;
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"[x,y] 걸린 시간 {end - now}");
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                if (r1 == 0 && r2 == 0)
+                    break;
             }
+            Console.WriteLine($"{count}번 만에 빠져 나옴");
         }
 
     }
