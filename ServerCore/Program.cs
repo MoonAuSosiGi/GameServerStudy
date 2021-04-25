7a8ac7edc1213fd042d116bc7ecc283cdd50bf93
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2021-04-25 11:24:38 +0900

    스레드 풀 사용 ( 최대 개수 초과하지 않음 )

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -16,10 +16,10 @@ namespace ServerCore
             ThreadPool.SetMinThreads(1, 1);
             ThreadPool.SetMaxThreads(5, 5);
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 4; i++)
                 ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
 
-            // 아래는 생성되지 않음.
+            // 위에서 최대 개수를 넘지 않게 했기 때문에, 아래는 생성됨
             ThreadPool.QueueUserWorkItem(MainThread);      
             while(true)
             {
