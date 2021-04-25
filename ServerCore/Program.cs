f3bc0ebdcfe551a6ca0455665b6a0c2cb9351ca6
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2021-04-25 13:07:28 +0900

    Race Condition 발생

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,51 +6,30 @@ namespace ServerCore
 {
     internal class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
-
+        static int number = 0;
+        
         static void Thread_1()
         {
-            y = 1; // Store y
-
-            // ---------------------
-            Thread.MemoryBarrier();
-
-            r1 = x; // Load x
+            for (int i = 0; i < 100000; i++)
+                number++;
         }
 
         static void Thread_2()
         {
-            x = 1; // Store x
-
-            // ---------------------
-            Thread.MemoryBarrier();
-
-            r2 = y; // Load y
+            for (int i = 0; i < 100000; i++)
+                number--;
         }
 
         public static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-                t1.Start();
-                t2.Start();
+            t1.Start();
+            t2.Start();
 
-                Task.WaitAll(t1, t2);
-
-                if (r1 == 0 && r2 == 0)
-                    break;
-            }
-            Console.WriteLine($"{count}번 만에 빠져 나옴");
+            Task.WaitAll(t1, t2);
+            Console.WriteLine(number);
         }
-
     }
 }
