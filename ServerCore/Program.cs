29d3ea559f99b077d287a8f9c34f95db7bb57c42
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2021-04-26 20:13:56 +0900

    Mutex를 사용한 lock, unlock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,36 +4,18 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class Lock
-    {
-        // 커널에 있는 bool 변수라고 보면 됨
-        ManualResetEvent _available = new ManualResetEvent(true);
-
-        public void Acquire()
-        {
-            _available.WaitOne(); // 입장 시도
-            // 자동으로 닫지 않으므로 수동으로 처리
-            _available.Reset();
-        }
-
-        public void Release()
-        {
-            _available.Set(); // 이벤트 상태 변경
-        }
-    }
-    
     internal class Program
     {
         static int _num = 0;
-        static Lock _lock = new Lock();
+        static Mutex _lock = new Mutex();
 
         static void Thread_1()
         {
             for(int i = 0; i < 100000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
@@ -41,9 +23,9 @@ namespace ServerCore
         {
             for (int i = 0; i < 100000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
