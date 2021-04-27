e3abeb09caa20740f43eff2f2c5f371c6b0749d4
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2021-04-27 21:18:13 +0900

    * ReaderWriterLock 구현

---
+++ ServerCore/Lock.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    // 재귀적 
+    class Lock
+    {
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK =  0x0000FFFF;
+        const int MAX_SPIN_COUNT = 5000;
+        // [Unused(1)] [WriterThreadId(15)] [ReadCount(16)]
+        int _flag = EMPTY_FLAG;
+        int _writeCount = 0;
+
+        public void WriteLock()
+        {
+            // 동일 스레드가 WriteLock을 이미 획득하고 있는지 확인 
+            int lockThreadid = (_flag & WRITE_MASK) >> 16;
+            if(Thread.CurrentThread.ManagedThreadId == lockThreadid)
+            {
+                _writeCount++;
+                return;
+            }
+
+            // 아무도 WriterLock or ReadLock을 획득하고 있지 않을 때, 경합해서 소유권을 얻음
+            int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+            while(true)
+            {
+                for(int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    // 시도해서 성공하면 return
+                    if(Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                    {
+                        _writeCount = 1;
+                        return;
+                    }
+                    
+                }
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnlock()
+        {
+            int lockCount = --_writeCount;
+            if(lockCount == 0)
+                Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+        }
+        
+        public void ReadLock()
+        {
+            // 동일 스레드가 WriteLock을 이미 획득하고 있는지 확인 
+            int lockThreadid = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadid)
+            {
+                Interlocked.Increment(ref _flag);
+                return;
+            }
+
+            // 아무도 WriteLock을 갖고 있지 않으면 ReadCount면 +1해줌
+            while (true)
+            {
+                for(int i =0; i < MAX_SPIN_COUNT; i++)
+                {
+                    int expected = (_flag & READ_MASK);
+                    if(Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                    {
+                        return;
+                    }
+                }
+                Thread.Yield();
+            }
+        }
+
+        public void ReadUnlock()
+        {
+            Interlocked.Decrement(ref _flag);
+        }
+    }
+}
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,40 +6,38 @@ namespace ServerCore
 {
     internal class Program
     {
-        static int _num = 0;
-        static Mutex _lock = new Mutex();
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
 
-        static void Thread_1()
-        {
-            for(int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
-        }
 
         public static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            Task t1 = new Task(delegate ()
+            {
+                for(int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();                    
+                    count++;                    
+                    _lock.WriteUnlock();
+                }
+            });
+
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
 
             t1.Start();
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine($"{_num}");
+            Console.WriteLine(count);
         }
     }
 }
