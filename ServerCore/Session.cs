2815c6165e763dab4b443f9533d7624ce39cff65
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2021-05-02 15:09:54 +0900

    Session 3 Send 개선

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -14,23 +14,22 @@ namespace ServerCore
         int _disconnected = 0;
 
         object _lock = new object();
-        Queue<byte[]> _sendQueue = new Queue<byte[]>();
-        bool _pending = false;
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();        
+        List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
         SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
-
+        SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
         public void Start(Socket socket)
         {
             _socket = socket;
 
-            SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
-            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            _recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
             // 여러 정보를 넘겨줄 수 있다.
             // recvArgs.UserToken = 1;
-            recvArgs.SetBuffer(new byte[1024], 0, 1024);
+            _recvArgs.SetBuffer(new byte[1024], 0, 1024);
 
             _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
 
-            RegisterRecv(recvArgs);
+            RegisterRecv();
         }
 
         public void Send(byte[] sendBuff)
@@ -39,7 +38,7 @@ namespace ServerCore
             {
                 _sendQueue.Enqueue(sendBuff);
 
-                if (_pending == false)
+                if (_pendingList.Count == 0)
                     RegisterSend();
             }
         }
@@ -58,9 +57,18 @@ namespace ServerCore
         #region 네트워크 통신 
         void RegisterSend()
         {
-            _pending = true;
-            byte[] buff =_sendQueue.Dequeue();
-            _sendArgs.SetBuffer(buff, 0, buff.Length);
+            // BufferList와 사용하면 에러
+            //_sendArgs.SetBuffer(buff, 0, buff.Length);
+
+            while (_sendQueue.Count > 0)
+            {
+                byte[] buff = _sendQueue.Dequeue();
+                // ArraySegment은 struct라 stack 영역에 할당됨. 따라서 값이 복사가 된다.
+                // 이런 형태로 넣어주면 안됨
+                //_sendArgs.BufferList.Add(new ArraySegment<byte>(buff, 0, buff.Length));
+                _pendingList.Add(new ArraySegment<byte>(buff, 0, buff.Length));
+            }
+            _sendArgs.BufferList = _pendingList;
 
             bool pending = _socket.SendAsync(_sendArgs);
 
@@ -78,12 +86,15 @@ namespace ServerCore
                 {
                     try
                     {
+                        _sendArgs.BufferList = null;
+                        _pendingList.Clear();
+
+                        Console.WriteLine($"Transferred bytes : {_sendArgs.BytesTransferred}");
+
                         if (_sendQueue.Count > 0)
                         {
                             RegisterSend();
                         }
-                        else
-                            _pending = false;
                     }
                     catch (Exception e)
                     {
@@ -97,13 +108,13 @@ namespace ServerCore
             }
         }
 
-        void RegisterRecv(SocketAsyncEventArgs args)
+        void RegisterRecv()
         {
-            bool pending = _socket.ReceiveAsync(args);
+            bool pending = _socket.ReceiveAsync(_recvArgs);
 
             if(pending == false)
             {
-                OnRecvCompleted(null, args);
+                OnRecvCompleted(null, _recvArgs);
             }
 
         }
@@ -118,7 +129,7 @@ namespace ServerCore
                 {
                     string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
                     Console.WriteLine($"[From Client] {recvData}");
-                    RegisterRecv(args);
+                    RegisterRecv();
                 }catch(Exception e)
                 {
                     Console.WriteLine($"OnRecvCompleted Failed {e}");
