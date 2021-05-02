b94a7193fe3785b1c61f450a9882c36dd63f0e01
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2021-05-02 16:34:22 +0900

    Connector 추가. 그에 맞춰 프로젝트 구성 변경 (서버 코어는 라이브러리로 변경됨)

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -3,9 +3,41 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using ServerCore;
 
 namespace DummyClient
 {
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            // 보낸다
+            for (int i = 0; i < 5; i++)
+            {
+                byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i} ");
+                Send(sendBuff);
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);            
+            Console.WriteLine($"[From Server] {recvData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes : {numOfBytes}");
+        }
+    }
+
     internal class Program
     {
         public static void Main(string[] args)
@@ -18,31 +50,15 @@ namespace DummyClient
             // 최종 주소와 포트번호 
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            
+            Connector connector = new Connector();
+
+            connector.Connect(endPoint, () => { return new GameSession(); });
+
             while (true)
-            {
-                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            {                
                 try
                 {
-                    // 서버에게 연결 요청
-                    socket.Connect(endPoint);
-                    Console.WriteLine($"[Connected To {socket.RemoteEndPoint.ToString()}");
-
-                    // 보낸다
-                    for(int i = 0; i < 5; i++)
-                    {
-                        byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i} ");
-                        int sendBytes = socket.Send(sendBuff);
-                    }
-
-                    // 받는다
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = socket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From Server] {recvData}");
-
-                    socket.Shutdown(SocketShutdown.Both);
-                    socket.Close();
+                
                 }
                 catch (Exception e)
                 {
