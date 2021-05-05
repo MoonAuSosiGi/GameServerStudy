6dd4a8f74abefc3dc69557db4cfa399ce0fd5c5b
MoonAuSosiGi::Desktop <moonausosigi@gmail.com>
2021-05-05 20:20:29 +0900

    Packet Generator 3 using 과 enum 추가 및 byte 추가

--- PacketGenerator/Program.cs
+++ PacketGenerator/Program.cs
@@ -7,6 +7,8 @@ namespace PacketGenerator
     class Program
     {
         private static string genPackets;
+        private static ushort packetId;
+        private static string packetEnums;
         static void Main(string[] args)
         {
             XmlReaderSettings settings = new XmlReaderSettings()
@@ -27,7 +29,9 @@ namespace PacketGenerator
                     {
                         ParsePacket(r);
                     }
-                    File.WriteAllText("GenPackets.cs", genPackets);
+
+                    string fileText = string.Format(PacketFormat.fileFormat, packetEnums, genPackets);
+                    File.WriteAllText("GenPackets.cs", fileText);
                 }
             }
         }
@@ -54,6 +58,8 @@ namespace PacketGenerator
             Tuple<string,string,string> t = ParseMembers(r);
             genPackets += string.Format(PacketFormat.packetFormat,
                 packetName, t.Item1, t.Item2, t.Item3);
+            packetEnums += string.Format(PacketFormat.packetEnumFormat, packetName, ++packetId) + Environment.NewLine +
+                           "\t";
         }
 
         // {1} 멤버 변수들
@@ -92,8 +98,13 @@ namespace PacketGenerator
                 string memberType = r.Name.ToLower();
                 switch (memberType)
                 {
-                    case "bool":
                     case "byte":
+                    case "sbyte":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readByteFormat, memberName, memberType);
+                        writeCode += string.Format(PacketFormat.writeByteFormat, memberName, memberType);
+                        break;
+                    case "bool":
                     case "short":
                     case "ushort":
                     case "int":
